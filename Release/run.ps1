feb66f4547131db86cde6132dc30bdaba6b1ea67
optimisme <optimisme@gmail.com>
2023-09-15 07:57:31 +0200

    Remove release

--- Release/run.ps1
+++
@@ -1,1 +0,0 @@
-java  --module-path "./lib/javafx-windows/lib" --add-modules javafx.controls,javafx.fxml -cp "Project.jar;.\lib\json-20220924.jar;.\lib\slf4j-api-2.0.3.jar;.\lib\Java-WebSocket-1.5.3.jar;.\lib\slf4j-simple-2.0.3.jar" Main
